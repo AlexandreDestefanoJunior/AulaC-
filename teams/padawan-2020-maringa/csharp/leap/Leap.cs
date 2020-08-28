3e22ee0590026ebbca473da057c8ee06d1e1b607
Alexandre <alex.des.jr@gmail.com>
2020-08-28 16:48:06 -0300

    Verificador de Ano

---
+++ teams/padawan-2020-maringa/csharp/leap/Leap.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        var leap = false;
+
+        if (year % 4 == 0 && year %100 != 0 || year % 400 == 0)
+        {
+
+            leap = true;
+
+
+        }
+
+        return leap;
+
+    }
+}
