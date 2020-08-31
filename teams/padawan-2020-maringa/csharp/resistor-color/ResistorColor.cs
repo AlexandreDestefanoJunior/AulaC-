821cd577a7b819c28f8adc21fdecd6da28353d92
Alexandre <alex.des.jr@gmail.com>
2020-08-31 08:55:35 -0300

    Exercicios Exercism

---
+++ teams/padawan-2020-maringa/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        int position = 0;
+        string[] a = new string[]
+            {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        for (int i = 0; i <= 9; i++)
+        {
+            if (a[i] == color)
+            {
+                position = i;
+            }
+
+        }
+        return position;
+    }
+
+    public static string[] Colors()
+        {
+            string[] a = new string[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+            return a;
+        }
+    
+}
